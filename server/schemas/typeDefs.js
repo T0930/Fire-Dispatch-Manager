accc65da19699c7e6719ad74011c31a04aec22dd
Tom M <T0930@protonmail.com>
2022-02-24 12:36:20 -0700

    added typeDefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,40 @@
-// const { gql } = require('apollo-server-express');
+
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Application {
+    _id: ID
+    companyName: String!
+    position: String!
+    dateApplied: String!
+    dateRejected: String
+    description: String
+    location: String
+    notes: [Note]
+    interviewDate: String
+    interviewTime: String
+    createdAt: String
+  }
+
+  type Note {
+    _id: ID
+    notes: String
+    createdAt: String
+  }
+
+  type Query {
+    applications: [Application]!
+    application(applicationId: ID!): Application
+  }
+
+  type Mutation {
+    addApplication(companyName: String!, position: String!, dateApplied: String!,
+      dateRejected: String, description: String, location: String,
+      interviewDate: String, interviewTime: String): Application
+    addNote(applicationId: ID!, notes: String!): Application
+    removeApplication(applicationId: ID!): Application
+    removeNote(applicationId: ID!, noteId: ID!): Application
+  }
+`;
+
+module.exports = typeDefs;
