6872edd68647e952b0562b9020c64bd288f201e6
Emily Burger <eburger939@gmail.com>
2022-02-28 15:23:57 -0600

    application page set up

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -18,6 +18,9 @@ mutation addApplication($companyName: String!, $position: String!, $dateApplied:
 //     }
 //   }
 
+
+
+
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
