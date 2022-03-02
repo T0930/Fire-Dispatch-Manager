1dd1d91303e286e74341e5b182b02c109e6f52e9
Emily Burger <eburger939@gmail.com>
2022-03-01 19:10:37 -0600

    pushing to heroku

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import Dashboard from "./components/Dashboard";
-import Application from "./pages/Application";
-// import Inactive from "./pages/Inactive";
 import './style.css'
 
 
